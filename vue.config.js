98fb4ef1ea0980bda1a642e243d904bdcf0c915a
d93fu6513 <102431864+d93fu6513@users.noreply.github.com>
2022-05-25 22:35:12 +0800

    add dist

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: 'popcorn-vue/dist'
+  publicPath: '/popcorn-vue/dist'
 })
